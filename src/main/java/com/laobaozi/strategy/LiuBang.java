67a96a9f11a92a46db67a9c887efb1c7392b9557
Jim <13681177@163.com>
2020-10-07 21:55:05 +0800

    strategy

--- src/main/java/com/laobaozi/strategy/LiuBang.java
+++ src/main/java/com/laobaozi/strategy/LiuBang.java
@@ -1,8 +1,8 @@
 package com.laobaozi.strategy;
 
-import com.laobaozi.strategy.impl.BackDoor;
-import com.laobaozi.strategy.impl.BlockEnemy;
-import com.laobaozi.strategy.impl.GivenGreenLight;
+import com.laobaozi.strategy.impl.ZhandaoChencang;
+import com.laobaozi.strategy.impl.Simianchuge;
+import com.laobaozi.strategy.impl.Lijian;
 
 public class ZhaoYun {
 
@@ -14,19 +14,19 @@ public class ZhaoYun {
 
         //刚刚到吴国的时候拆第一个
         System.out.println("-----------刚刚到吴国的时候拆第一个-------------");
-        context = new Context(new BackDoor()); //拿到妙计
+        context = new Context(new ZhandaoChencang()); //拿到妙计
         context.operate(); //拆开执行
         System.out.println("\n\n\n\n");
 
         //刘备乐不思蜀了，拆第二个了
         System.out.println("-----------刘备乐不思蜀了，拆第二个了-------------");
-        context = new Context(new GivenGreenLight());
+        context = new Context(new Lijian());
         context.operate(); //执行了第二个锦囊了
         System.out.println("\n\n\n\n");
 
         //孙权的小兵追了，咋办？拆第三个
         System.out.println("-----------孙权的小兵追了，咋办？拆第三个了-------------");
-        context = new Context(new BlockEnemy());
+        context = new Context(new Simianchuge());
         context.operate(); //孙夫人退兵
         System.out.println("\n\n\n\n");
 
